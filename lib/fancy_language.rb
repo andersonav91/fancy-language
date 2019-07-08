1f9ec7e7efadb46c186dcf5e4ea02f7d89f88316
Anderson Alvarez V <andersonav91@gmail.com>
2019-07-08 15:30:18 -0500

    * Added methods for reading language files in the app and the plugins. * Added method for obtain all languages in the app.

--- lib/fancy_language.rb
+++ lib/fancy_language.rb
@@ -1,14 +1,30 @@
 
 require 'fancy_language/hooks/views_layouts_hook'
+require 'yaml'
 
 module FancyLanguage
 
   SOME_CONSTANT = 1
 
   class << self
-    def some_method
 
+    include Redmine::I18n
+
+    def read_app_language_file_content(language = :en)
+      content = YAML.load_file(File.join(Rails.root, 'config', 'locales', "#{language}.yml"))
+      content
     end
+
+    def read_plugin_language_file_content(plugin, language = :en)
+      plugin = Redmine::Plugin.find(plugin)
+      content = YAML.load_file(File.join(plugin.directory, 'config', 'locales', "#{language}.yml"))
+      content
+    end
+    
+    def app_languages
+      valid_languages
+    end
+    
   end
 
 end
