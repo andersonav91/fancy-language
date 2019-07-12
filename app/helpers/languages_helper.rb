265611eeb537292832232b484d201bb4a2baf056
Anderson Alvarez V <andersonav91@gmail.com>
2019-07-11 20:15:45 -0500

    * Added code for printing the yml tree file.

--- app/helpers/languages_helper.rb
+++ app/helpers/languages_helper.rb
@@ -1,2 +1,17 @@
 module LanguagesHelper
+
+  def print_hash(h, spaces = 12, level = 0)
+    tree = ""
+    h.each do |key, val|
+      format = content_tag(:span, "#{key}:", {style: "font-weight: bold;"})
+      if val.is_a? Hash
+        tree << content_tag(:div, format, {style: "padding-left: #{spaces * level}px;"})
+        tree << print_hash(val, spaces, level + 1)
+      else
+        tree << content_tag(:div, format + " " + val.to_s, {style: "padding-left: #{spaces * level}px;"})
+      end
+    end
+    tree
+  end
+
 end
